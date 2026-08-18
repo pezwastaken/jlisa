88b6262b244cea16e538f65aed7df3a070284680
Andrea Pezzuolo <andrea.pezzuolo@studenti.unipr.it>
2026-08-18 22:35:50 +0200

    return top in case of comparison between weak identifiers

--- jlisa/src/main/java/it/unive/jlisa/program/cfg/expression/JavaComparisonEqual.java
+++ jlisa/src/main/java/it/unive/jlisa/program/cfg/expression/JavaComparisonEqual.java
@@ -1,6 +1,8 @@
 package it.unive.jlisa.program.cfg.expression;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import it.unive.jlisa.program.type.JavaClassType;
 import it.unive.jlisa.program.type.JavaReferenceType;
@@ -12,7 +14,6 @@ import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.StatementStore;
 import it.unive.lisa.interprocedural.InterproceduralAnalysis;
 import it.unive.lisa.lattices.ExpressionSet;
-import it.unive.lisa.lattices.Satisfiability;
 import it.unive.lisa.program.cfg.CFG;
 import it.unive.lisa.program.cfg.CodeLocation;
 import it.unive.lisa.program.cfg.statement.Expression;
@@ -23,6 +24,7 @@ import it.unive.lisa.symbolic.heap.HeapDereference;
 import it.unive.lisa.symbolic.value.BinaryExpression;
 import it.unive.lisa.symbolic.value.Constant;
 import it.unive.lisa.symbolic.value.GlobalVariable;
+import it.unive.lisa.symbolic.value.Identifier;
 import it.unive.lisa.symbolic.value.PushAny;
 import it.unive.lisa.symbolic.value.operator.binary.ComparisonEq;
 import it.unive.lisa.type.Type;
@@ -44,7 +46,7 @@ public class JavaComparisonEqual extends Equal {
 	@Override
 	protected int compareSameClassAndParams(
 			Statement o) {
-		return 0; // no extra fields to compare
+		return 0;
 	}
 
 	@Override
@@ -75,7 +77,16 @@ public class JavaComparisonEqual extends Equal {
 				c = new Constant(BoolType.INSTANCE, false, location);
 			}
 			else if (setl.size() == 1 && setr.size() == 1 && setl.equals(setr)) {
-				c = new Constant(BoolType.INSTANCE, true, location);
+				SymbolicExpression l = setl.iterator().next();
+				SymbolicExpression r = setr.iterator().next();
+				if (l instanceof Identifier l2 && r instanceof Identifier r2) {
+					if (l2.isWeak() || r2.isWeak())
+						c = new PushAny(BoolType.INSTANCE, location);
+					else
+						c = new Constant(BoolType.INSTANCE, true, location);
+				}
+				else
+					c = new Constant(BoolType.INSTANCE, true, location);
 			}
 			else {
 				c = new PushAny(BoolType.INSTANCE, location);
