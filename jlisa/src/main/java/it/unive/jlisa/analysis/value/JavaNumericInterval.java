659d79956685e4492a03282ab892c37832d0a843
= <karimouhamou@gmail.com>
2026-08-26 23:04:03 +0200

    fix: skip interval assume on boolean comparisons (if (x == true) no longer yields bottom)

--- jlisa/src/main/java/it/unive/jlisa/analysis/value/JavaNumericInterval.java
+++ jlisa/src/main/java/it/unive/jlisa/analysis/value/JavaNumericInterval.java
@@ -645,4 +645,19 @@ public class JavaNumericInterval extends Interval {
 		return super.assume(environment, expression, src, dest, oracle);
 	}
 
+	@Override
+	public ValueEnvironment<IntInterval> assumeBinaryExpression(
+			ValueEnvironment<IntInterval> environment,
+			BinaryExpression expression,
+			ProgramPoint src,
+			ProgramPoint dest,
+			SemanticOracle oracle)
+			throws SemanticException {
+		if (expression.getStaticType().isBooleanType())
+			// i confronti booleani li decide la ConstantPropagation,
+			// l'interval non deve toccare lo stato (altrimenti `true` -> BOTTOM)
+			return environment;
+		return super.assumeBinaryExpression(environment, expression, src, dest, oracle);
+	}
+
 }
