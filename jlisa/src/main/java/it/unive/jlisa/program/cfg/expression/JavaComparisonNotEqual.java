fe00e50c588799c08479b8bc8a929e0636013591
Andrea Pezzuolo <andrea.pezzuolo@studenti.unipr.it>
2026-08-27 13:11:35 +0200

    fix javanoteq recomputing semantics of child expressions

--- jlisa/src/main/java/it/unive/jlisa/program/cfg/expression/JavaComparisonNotEqual.java
+++ jlisa/src/main/java/it/unive/jlisa/program/cfg/expression/JavaComparisonNotEqual.java
@@ -1,7 +1,10 @@
 package it.unive.jlisa.program.cfg.expression;
 
+import it.unive.jlisa.program.type.JavaClassType;
+import it.unive.jlisa.program.type.JavaReferenceType;
 import it.unive.lisa.analysis.AbstractDomain;
 import it.unive.lisa.analysis.AbstractLattice;
+import it.unive.lisa.analysis.Analysis;
 import it.unive.lisa.analysis.AnalysisState;
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.StatementStore;
@@ -11,9 +14,16 @@ import it.unive.lisa.program.cfg.CodeLocation;
 import it.unive.lisa.program.cfg.statement.Expression;
 import it.unive.lisa.program.cfg.statement.Statement;
 import it.unive.lisa.symbolic.SymbolicExpression;
-
+import it.unive.lisa.symbolic.heap.AccessChild;
+import it.unive.lisa.symbolic.heap.HeapDereference;
+import it.unive.lisa.symbolic.value.BinaryExpression;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.GlobalVariable;
+import it.unive.lisa.symbolic.value.operator.binary.ComparisonNe;
+import it.unive.lisa.type.Type;
+import it.unive.lisa.type.Untyped;
 import it.unive.lisa.program.cfg.statement.comparison.NotEqual;
-import it.unive.lisa.program.cfg.statement.logic.Not;
+import it.unive.lisa.program.type.BoolType;
 
 public class JavaComparisonNotEqual extends NotEqual {
 
@@ -48,13 +58,65 @@ public class JavaComparisonNotEqual extends NotEqual {
 			StatementStore<A> expressions)
 			throws SemanticException {
 
+		Analysis<A, D> analysis = interprocedural.getAnalysis();
+		boolean bothRef = (left.getStaticType().isReferenceType() || left.getStaticType().isNullType()) && (right.getStaticType().isReferenceType() || right.getStaticType().isNullType());
+		boolean bothPrimitive = !left.getStaticType().isReferenceType() && !right.getStaticType().isReferenceType();
+
+		if (bothPrimitive || bothRef) {
+			return super.fwdBinarySemantics(interprocedural, state, left, right, expressions);
+		}
+
+		AnalysisState<A> res = state.bottomExecution();
+		if (left.getStaticType().isReferenceType()) {
+			res = notEqWithUnbox(analysis, state, left, right, expressions);
+		}
+		else {
+			res = notEqWithUnbox(analysis, state, right, left, expressions);
+		}
+		return res;
+
+	}
+
+	private <A extends AbstractLattice<A>, D extends AbstractDomain<A>> AnalysisState<A> notEqWithUnbox(
+			Analysis<A, D> analysis,
+			AnalysisState<A> state,
+			SymbolicExpression x,
+			SymbolicExpression y,
+			StatementStore<A> expressions)
+			throws SemanticException {
+
+		AnalysisState<A> res = state.bottomExecution();
+		Type t = x.getStaticType();
+
+		if (t instanceof JavaReferenceType jrt && jrt.getInnerType() instanceof JavaClassType innerType) {
+
+			assert(JavaClassType.isWrapperType(innerType));
+			SymbolicExpression unboxed = unbox(analysis, state, x, innerType, expressions);
+
+			AnalysisState<A> tmp = analysis.smallStepSemantics(state, new BinaryExpression(getStaticType(), unboxed, y, ComparisonNe.INSTANCE, getLocation()), this);
+			res = res.lub(tmp);
+		}
+		else {
+			Constant c = new Constant(BoolType.INSTANCE, true, getLocation());
+			res = res.lub(analysis.smallStepSemantics(state, c, this));
+		}
+		return res;
+	}
+
+	private <A extends AbstractLattice<A>, D extends AbstractDomain<A>> SymbolicExpression unbox(
+			Analysis<A, D> analysis,
+			AnalysisState<A> state,
+			SymbolicExpression toUnbox,
+			JavaClassType t,
+			StatementStore<A> expressions)
+			throws SemanticException {
 		CodeLocation location = getLocation();
+		assert(toUnbox.getStaticType().isReferenceType());
 
-		JavaComparisonEqual eq = new JavaComparisonEqual(getCFG(), location, getLeft(), getRight());
-		Not not = new Not(getCFG(), location, eq);
-		AnalysisState<A> tmp = not.forwardSemantics(state, interprocedural, expressions);
-
-		return tmp;
+		GlobalVariable g = new GlobalVariable(Untyped.INSTANCE, "value", location);
+		HeapDereference deref = new HeapDereference(t, toUnbox, location);
+		AccessChild a = new AccessChild(JavaClassType.getUnwrappedType(t), deref, g, location);
+		return a;
 	}
 
 }
